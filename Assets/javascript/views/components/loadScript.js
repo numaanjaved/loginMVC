18f9e8b16ff8664da43af0779f8291f1ae9ad831
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-20 16:28:02 +0500

    minor changes in script function

--- Assets/javascript/views/components/loadScript.js
+++ Assets/javascript/views/components/loadScript.js
@@ -2,12 +2,8 @@ let scriptCreate = (scriptName, src, timeout) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             let script = createNewElement(["script", null, "head", null, { src: src }])
-            script.onload = () => {
-                resolve(`${scriptName} loaded successfully`);
-            }
-            script.onerror = () => {
-                reject(`Error while loading ${scriptName}`);
-            }
+            script.onload = () => { resolve(`${scriptName} loaded successfully`); }
+            script.onerror = () => { reject(`Error while loading ${scriptName}`); }
         }, timeout);
     });
-}
+};
