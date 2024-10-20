62d422e3af930b998efefed831d13d72fb155678
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-20 18:33:30 +0500

    cookie moved to indexController

--- Assets/javascript/controllers/indexController/indexScripts.js
+++ Assets/javascript/controllers/indexController/indexScripts.js
@@ -1,5 +1,5 @@
 let runScripts = async () => {
-    let checkAuth = await scriptCreate("Authorization Checking Script", "./Assets/javascript/views/auth.js", 10);
+    let checkAuth = await scriptCreate("Authorization Checking Script", "./Assets/javascript/controllers/indexController/auth.js", 10);
     return checkAuth;
 }
 runScripts().then(val => console.log(val)).catch(err => console.log(err));
