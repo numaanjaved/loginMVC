cbdd603c6e061c720351a810964ea824a6d5d591
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-20 16:08:48 +0500

    scripts errors while loading fixed

--- Assets/javascript/views/components/loadScript.js
+++ Assets/javascript/views/components/loadScript.js
@@ -1,34 +1,13 @@
-let loadScript = async (src, scriptName, scriptLoadingTime) => {
-    let scriptsArray = [];
-    scriptsArray.push(scriptName);
-    let head = document.getElementsByTagName("head")[0];
-    let returnScripts = [];
-
-    for (let i = 0; i < scriptsArray.length; i++) {
-        let scriptPromise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                let newScript = createNewElement(["script", null, head, null, { src: `${src}`, defer: true }]);
-                newScript.onload = () => {
-                    resolve(`"${scriptName}" Script Loaded Successfully`);
-                };
-                newScript.onerror = () => {
-                    reject(`Error Occurred while Loading "${scriptName}" Script`);
-                };
-            }, scriptLoadingTime);
-        });
-        returnScripts.push(await scriptPromise);
-    }
-    return returnScripts;
-};
-
-let runScript = async (src, scriptName, time) => {
-    let scriptsArray = [];
-    scriptsArray.push(loadScript(src, scriptName, time));
-    for (let i = 0; i < scriptsArray.length; i++) {
-        await scriptsArray[i].then((value) => {
-            value.forEach((val) => console.log(val));
-        }).catch((err) => {
-            console.error(err);
-        });
-    }
-};
+let scriptCreate = (scriptName, src, timeout) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let script = createNewElement(["script", null, "head", null, { src: src }])
+            script.onload = () => {
+                resolve(`${scriptName} loaded successfully`);
+            }
+            script.onerror = () => {
+                reject(`Error while loading ${scriptName}`);
+            }
+        }, timeout);
+    });
+}
