4522c4cf0d62c82cdecdc19d68e229cd4770d0ec
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-21 10:54:42 +0500

    reset img fixed

--- Assets/javascript/views/form/formReset.js
+++ Assets/javascript/views/form/formReset.js
@@ -2,6 +2,7 @@ let fromReset = () => {
     form.reset();
     adminAttContainer.style.display = "none";
     admin_heading.style.display = "none";
+    imgDisplay.src = "./Assets/images/default_profile.png";
     let select = document.getElementById(`select_user`);
     let selectUserHeading = document.getElementById(`choose_user_heading`);
     select.style.display = "flex";
