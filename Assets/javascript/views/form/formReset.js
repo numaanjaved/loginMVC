fca345ba805c3552d4c951bdc122a16c822e0e3e
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-21 10:52:46 +0500

    update button reset bug fixed

--- Assets/javascript/views/form/formReset.js
+++ Assets/javascript/views/form/formReset.js
@@ -2,9 +2,12 @@ let fromReset = () => {
     form.reset();
     adminAttContainer.style.display = "none";
     admin_heading.style.display = "none";
-    imgDisplay.src = "./Assets/images/default_profile.png";
     let select = document.getElementById(`select_user`);
     let selectUserHeading = document.getElementById(`choose_user_heading`);
     select.style.display = "flex";
     selectUserHeading.style.display = "block";
 };
+let resetBtn = document.getElementById(`reset_btn`);
+resetBtn.addEventListener("click", () => {
+    userIndexCheck = null;
+})
