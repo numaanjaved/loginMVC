e960e999978a9e93c161420634a8d1b8ea330726
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-22 21:35:33 +0500

    index bug fixed changed code flow

--- Assets/javascript/controllers/indexController/index.js
+++ Assets/javascript/controllers/indexController/index.js
@@ -2,9 +2,13 @@ let indexScript = async () => {
     let script = await scriptCreate("index Scripts", "./Assets/javascript/controllers/indexController/scripts.js", 0);
     return script;
 }
-indexScript().then(val => console.log(val)).catch(err => console.log(err));
-setTimeout(() => {
-    if (loggedInUser()) {
-        window.location.href = "/home.html";
-    } else { window.location.href = "/login.html"; }
-}, 150);
+
+indexScript().then(() => {
+    runScripts().then(() => {
+        if (loggedInUser()) {
+            window.location.href = "/home.html";
+        } else {
+            window.location.href = "/login.html";
+        }
+    });
+}).catch(err => console.log(err));
