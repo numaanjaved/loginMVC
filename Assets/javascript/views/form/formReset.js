2e18fcfc232507745480f7d134eaa154a9b99ffc
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-22 21:04:27 +0500

    Create user bug fixed

--- Assets/javascript/views/form/formReset.js
+++ Assets/javascript/views/form/formReset.js
@@ -7,6 +7,14 @@ let fromReset = () => {
     let selectUserHeading = document.getElementById(`choose_user_heading`);
     select.style.display = "flex";
     selectUserHeading.style.display = "block";
+    attributes = [
+        { attrName: userFirstName, regex: /^[a-zA-Z\s]*$/, length: 30 },
+        { attrName: userLastName, regex: /^[a-zA-Z\s]*$/, length: 30 },
+        { attrName: userEmail, regex: /^[a-zA-Z0-9]+(?:[._][a-zA-Z0-9]+)*@[A-Za-z]+\.[A-Za-z]{2,}$/, length: 100 },
+        { attrName: userContactNumber, regex: /^[0-9]{2,}[0-9]{7,}$/, length: 20 },
+        { attrName: userAddress, regex: /^[a-zA-Z0-9\s,.'-]*$/, length: 150 },
+        { attrName: userBio, regex: /^[a-zA-Z0-9\s,.'-]*$/, length: 300 },
+    ];
 };
 let resetBtn = document.getElementById(`reset_btn`);
 resetBtn.addEventListener("click", () => {
